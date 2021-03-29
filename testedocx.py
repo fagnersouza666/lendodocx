981fdb91cf3583cbcedb82fbc05bc781467c3829
Fagner Souza <fagner-souza@procergs.rs.gov.br>
2021-03-29 07:43:46 -0300

    first commit

---
+++ testedocx.py
@@ -0,0 +1,16 @@
+from zipfile import ZipFile
+from urllib.request import urlopen
+from io import BytesIO
+from bs4 import BeautifulSoup
+
+wordFile = open('teste-word.docx','r+b').read()
+wordFile = BytesIO(wordFile)
+document = ZipFile(wordFile)
+
+xmlContent = document.read('word/document2.xml')
+
+wordObj = BeautifulSoup(xmlContent, 'xml')
+textStrings = wordObj.find_all('w:t')
+
+for textElem in textStrings:
+    print(textElem.text)
